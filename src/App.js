1281cc9b8fac0a11b331e179b358d74eb470087e
davidcassalstudio <davidcassal@gmail.com>
2023-03-14 23:43:49 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import { useLayoutEffect, useState } from 'react';
 import './App.css';
+import Contato from './components/contato';
 
 function App() {
+  const [ users, setUsers ] = useState([]);
+
+  useLayoutEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(resp => resp.json())
+      .then(data => {
+        setUsers(data)
+      })
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <> 
+      <section className='grid'>
+        {
+          users?.map(user => (
+            <div className='card' key={user._id}>
+              <h2>{user.nome}</h2>
+              <ul>
+                <li><strong>E-mail:</strong> {user.email}</li>
+                <li><strong>Telefone:</strong> {user.telefone}</li>
+              </ul>
+            </div>
+          ))
+        }
+      </section>
+
+      <Contato />
+    </>
+  )
 }
 
 export default App;
+
+
+// import { useLayoutEffect, useState } from 'react';
+// import './App.css';
+
+// function App() {
+//   const [ users, setUsers ] = useState([]);
+
+//   useLayoutEffect(() => {
+//     fetch('http://localhost:5000/users')
+//       .then(resp => resp.json())
+//       .then(data => {
+//         setUsers(data)
+//       })
+//   },[])
+
+//   return (
+//     <section className='grid'>
+//       {
+//         users?.map(user => (
+//           <div className='card' key={user._id}>
+//             <h2>{user.nome}</h2>
+//             <p>{user.email}</p>
+//             <p>{user.telefone}</p>
+//           </div> 
+//         ))
+//       }
+//     </section>
+//   );
+// }
+// export default App;
---
+++ src/components/contato.jsx
@@ -0,0 +1,46 @@
+//import { useState } from 'react';
+import { useState } from 'react';
+import '../App.css';
+
+function Contato() {
+    const [ userInput, setuserInput ] = useState([]);
+
+    const onHandleInput = (e) => {
+        const name  = e.target.name;
+        const value = e.target.value;
+
+        setuserInput({
+            ...userInput,
+            [name]: value
+        })
+    }
+
+    const onHandleSubmit = (e) => {
+        e.preventDefault();
+        window.location.reload();
+        Array.from(document.querySelectorAll('.input-form')).map(input => input.value = '');
+
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers:  {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userInput)
+        })
+    }
+
+    return (
+        <section className='contact-create'>
+            <h2>Cadastrar usuário</h2>
+            <form method="POST" onSubmit={onHandleSubmit}>
+                <input type="text" className="input-form" name="nome" placeholder="Digite seu nome" onInput={onHandleInput} />
+                <input type="email" className="input-form" name="email" placeholder="exemplo@gmail.com" onInput={onHandleInput} />
+                <input type="tel" className="input-form" name="telefone" placeholder="(11)98877-6655" onInput={onHandleInput} />
+                <button type="submit" className="submit-form" name="cadastrar">Cadastrar</button>
+            </form>
+        </section>
+    )
+}
+
+export default Contato;
